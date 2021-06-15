2e8ac507445a2b97244c03f87c104fad4dab2470
Vinicius PC <viniciuspradocarneiro@gmail.com>
2021-06-15 15:43:03 -0300

    Ligar

--- src/client/events/ready.js
+++ src/client/events/ready.js
@@ -5,18 +5,21 @@ module.exports = async (client) => {
   const status = [
     {
       name: `👤 Usuários: ${client.users.cache.size} `,
+      type: "WATCHING"
     },
     {
       name: `🏠 Servidores: ${client.guilds.cache.size}`,
+      type: "PLAYING"
     },
   ];
 
   client.user.setStatus("idle")
 
-  function setStatus() {
+  function setMsg() {
     var randomStatus = status[Math.floor(Math.random() * status.length)];
     client.user.setActivity(randomStatus.name);
   }
 
+  client.user.setStatus("idle")
 
 };
